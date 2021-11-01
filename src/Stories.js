bd0bcfae38ca085f5a18a108a9757e280b797c37
Edwin Torres <edwin_torres@icloud.com>
2021-10-31 20:27:07 -0400

    Completed CSS styling.

--- src/Stories.js
+++ src/Stories.js
@@ -8,7 +8,23 @@ class Stories extends Component {
     return (
       <div>
         {stories.map((story) => {
-          return <div key={story.id}>{story.title}</div>;
+          return (
+            <div className='container' key={story.id}>
+              <div className='div1'>
+                <a href={story.link}>
+                  <img id='img' src={story.img} alt='' />
+                </a>
+              </div>
+              <div className='div2'>
+                <a href={story.link}>
+                  <strong id='title'>{story.title}</strong> <strong id='date'>{story.date}</strong>
+                </a>
+              </div>
+              <div className='div3'>
+                <p id='text'>{story.text}</p>
+              </div>
+            </div>
+          );
         })}
       </div>
     );
